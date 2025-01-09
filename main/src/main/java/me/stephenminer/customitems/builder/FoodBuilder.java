0eebe5dc65b38b8f056563625d3faeaef9a110b9
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2025-01-09 11:17:22 -0800

    Add Food component compatibility for versions 1.20.5-1.21.3

---
+++ main/src/main/java/me/stephenminer/customitems/builder/FoodBuilder.java
@@ -0,0 +1,10 @@
+package me.stephenminer.customitems.builder;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public interface FoodBuilder {
+
+    ItemStack build();
+
+}
